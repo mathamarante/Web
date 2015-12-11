589d6ca9a77d805b2a0bf5ec38abffb688b1300c
Matheus Forte Amarante <matheus@ats.com.br>
2015-12-11 18:19:58 -0200

    RASTREAMENTO DE CARGA

---
+++ Frota/Stn/Site/js/script.js
@@ -0,0 +1,25 @@
+﻿function enterTeclado2(lcTxAtua, lcTxFocu) {
+    $("#" + lcTxAtua).on("keyup", function (e) {
+        var theEvent = e || window.event;
+        var keyPressed = theEvent.keyCode || theEvent.which;
+        if (keyPressed == 13) {
+            document.getElementById(lcTxFocu).focus();
+        }
+        return true;
+    });
+}
+
+function enterTeclado3(lcTxAtua, lcTxFocu, lcAhClic) {
+    $("#" + lcTxAtua).on("keyup", function (e) {
+        var theEvent = e || window.event;
+        var keyPressed = theEvent.keyCode || theEvent.which;
+        if (keyPressed == 13) {
+            document.getElementById(lcTxFocu).focus();
+
+            if (typeof document.getElementById(lcAhClic).onclick == "function") {
+                document.getElementById(lcAhClic).onclick.apply(document.getElementById(lcAhClic));
+            }
+        }
+        return true;
+    });
+}
